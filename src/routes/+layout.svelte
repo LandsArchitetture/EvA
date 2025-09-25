1af59a64c758b4af7c12d51247ede13d331f8438
Adriano Saurwein <nano2101@outlook.com>
2025-09-25 11:41:33 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import logo from '$lib/assets/logo.jpg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={logo} />
+</svelte:head>
+
+{@render children?.()}

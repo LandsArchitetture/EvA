c63483d024145d058dd6d24da5fb570c05d18c53
Adriano Saurwein <nano2101@outlook.com>
2025-09-25 11:49:34 +0200

    Changed Icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import logo from '$lib/assets/logo.jpg';
+	import logo from '$lib/assets/logo.png';
 
 	let { children } = $props();
 </script>
